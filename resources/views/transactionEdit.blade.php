5f6a0dbb0b511159d0820612a0b0f393b37e3301
Léa Valmy <lea.valmy@gmail.com>
2023-07-25 16:42:46 +0200

    us5 ok HTML mutualized

--- resources/views/transactionEdit.blade.php
+++ resources/views/transactionEdit.blade.php
@@ -1,53 +1,11 @@
-<!DOCTYPE html>
-<html lang="fr">
-
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Ajouter une opération - Mes Comptes</title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
-</head>
-
-<body>
-    <div class="container-fluid">
-        <header class="row flex-wrap justify-content-between align-items-center p-3 mb-4 border-bottom">
-            <a href="index.html" class="col-1">
-                <i class="bi bi-piggy-bank-fill text-primary fs-1"></i>
-            </a>
-            <nav class="col-11 col-md-7">
-                <ul class="nav">
-                    <li class="nav-item">
-                        <a href="index.html" class="nav-link link-secondary" aria-current="page">Opérations</a>
-                    </li>
-                    <li class="nav-item">
-                        <a href="summary.html" class="nav-link link-body-emphasis">Synthèses</a>
-                    </li>
-                    <li class="nav-item">
-                        <a href="categories.html" class="nav-link link-body-emphasis">Catégories</a>
-                    </li>
-                    <li class="nav-item">
-                        <a href="import.html" class="nav-link link-body-emphasis">Importer</a>
-                    </li>
-                </ul>
-            </nav>
-            <form action="" class="col-12 col-md-4" role="search">
-                <div class="input-group">
-                    <input type="text" class="form-control" placeholder="Rechercher..."
-                        aria-describedby="button-search">
-                    <button class="btn btn-primary" type="submit" id="button-search">
-                        <i class="bi bi-search"></i>
-                    </button>
-                </div>
-            </form>
-        </header>
-    </div>
+@extends ('page')
+@section('title', 'Modifier une opération - Mes Comptes')
+@section('content')
 
     <div class="container">
         <section class="card mb-4 rounded-3 shadow-sm">
             <div class="card-header py-3">
-                <h1 class="my-0 fw-normal fs-4">Ajouter une opération</h1>
+                <h1 class="my-0 fw-normal fs-4">Modifier une opération</h1>
             </div>
             <div class="card-body">
                 <form action=" {{ route('transaction.update', $transaction->id) }} " method="POST" >
@@ -79,12 +37,4 @@
     </div>
 
    
-
-    <footer class="py-3 mt-4 border-top">
-        <p class="text-center text-body-secondary">© 2023 Mes comptes</p>
-    </footer>
-
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
-</body>
-
-</html>
+@endsection
