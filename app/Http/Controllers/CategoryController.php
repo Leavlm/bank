3158ab213c06958237ddc075397b8db1fa22454a
Tobias Doboszewicz <tobias13@hotmail.fr>
2023-07-26 12:11:55 +0200

    add controller for category

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
 use Illuminate\Http\Request;
 
 class CategoryController extends Controller
@@ -13,7 +14,11 @@ class CategoryController extends Controller
      */
     public function index()
     {
-        //
+        $data = [
+            'icon' => Category::all('icon_class'),
+            'categorie_name' => Category::all('icon_class')
+        ];
+        return view('bank', $data);
     }
 
     /**
