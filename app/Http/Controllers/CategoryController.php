42b50461ada8acca17db31d7d472af424b242465
Tobias Doboszewicz <tobias13@hotmail.fr>
2023-07-26 15:49:04 +0200

    categorie ok

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -14,12 +14,17 @@ class CategoryController extends Controller
      */
     public function index()
     {
+
+        var_dump(
+            Category::find(1)->transactions   
+        );
+        exit;
         // $categoryIds = Category::where('categorie_name', Category::all('categorie_name'))->pluck('id');
         $data = [
-            'icon' => Category::all('icon_class'),
+            // 'icon' => Category::all('icon_class'),
             'categories' => Category::all(),
-            'id' => Category::all('id'),
-            'count' => Category::all()->count() 
+            // 'id' => Category::all('id'),
+            // 'count' => Category::all()->count() 
 
         ];
         return view('categories', $data);
